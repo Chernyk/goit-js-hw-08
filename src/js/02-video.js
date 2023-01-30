899c6c26a1f086ec62035c604fb0ff55431ca41c
Oleh <olegchernyk1993@gmail.com>
2023-01-30 12:06:50 +0200

    change task 2, 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,14 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+const LOCALSTORAGEYIME_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 player.on(
   'timeupdate',
   throttle(function (time) {
-    localStorage.setItem('videoplayer-current-time', time.seconds);
+    localStorage.setItem(LOCALSTORAGEYIME_KEY, time.seconds);
   }, 1000)
 );
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+player.setCurrentTime(localStorage.getItem(LOCALSTORAGEYIME_KEY || 0));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,12 +1,10 @@
 import throttle from 'lodash.throttle';
 const STORAGE_KEY = 'feedback-form-state';
-const formData = {};
+let formData = {};
 const refs = {
   form: document.querySelector('.feedback-form'),
-  textarea: document.querySelector('.feedback-form textarea'),
-  input: document.querySelector('.feedback-form input'),
 };
-refs.form.addEventListener('submit', onFornSubmit);
+refs.form.addEventListener('submit', onFormSubmit);
 refs.form.addEventListener('input', throttle(onInput, 500));
 function onInput(evt) {
   formData[evt.target.name] = evt.target.value;
@@ -15,18 +13,22 @@ function onInput(evt) {
 
 saveInfoAfterRefresh();
 
-function onFornSubmit(evt) {
+function onFormSubmit(evt) {
   evt.preventDefault();
   console.log(formData);
   evt.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
 }
 function saveInfoAfterRefresh() {
-  const saveMessage = localStorage.getItem(STORAGE_KEY);
-  if (saveMessage) {
-    const parseMessage = JSON.parse(saveMessage);
-    console.log(parseMessage);
-    refs.input.value = parseMessage.email;
-    refs.textarea.value = parseMessage.message;
+  try {
+    const saveMessage = localStorage.getItem(STORAGE_KEY);
+    if (saveMessage) {
+      formData = JSON.parse(saveMessage);
+      Object.entries(formData).forEach(([key, value]) => {
+        refs.form[key].value = value;
+      });
+    }
+  } catch (error) {
+    console.log(error);
   }
 }
