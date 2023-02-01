b8c693c27ae5d02305f740ec5e15c970c0ecc4fc
Oleh <olegchernyk1993@gmail.com>
2023-02-01 11:32:27 +0200

    change task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,14 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const LOCALSTORAGEYIME_KEY = 'videoplayer-current-time';
+const LOCALSTORAGETIME_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+
 player.on(
   'timeupdate',
   throttle(function (time) {
-    localStorage.setItem(LOCALSTORAGEYIME_KEY, time.seconds);
+    localStorage.setItem(LOCALSTORAGETIME_KEY, time.seconds);
   }, 1000)
 );
-
-player.setCurrentTime(localStorage.getItem(LOCALSTORAGEYIME_KEY || 0));
+player.setCurrentTime(localStorage.getItem(LOCALSTORAGETIME_KEY) || 0);
